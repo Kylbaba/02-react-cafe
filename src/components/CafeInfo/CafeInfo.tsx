b6b8b9d28a3942aa34d46f4177770ffac23661b1
Tatiana <petrenkotanya18@gmail.com>
2025-06-10 17:28:59 +0300

    completed the tasks

--- src/components/CafeInfo/CafeInfo.tsx
+++ src/components/CafeInfo/CafeInfo.tsx
@@ -1,8 +1,15 @@
-{
-  /* <div className={css.container}>
-  <h1 className={css.title}>Sip Happens Café</h1>
-  <p className={css.description}>
-    Please rate our service by selecting one of the options below.
-  </p>
-</div>; */
-}
+import React from "react";
+import css from "./CafeInfo.module.css";
+
+const CafeInfo: React.FC = () => {
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>Sip Happens Café</h1>
+      <p className={css.description}>
+        Please rate our service by selecting one of the options below.
+      </p>
+    </div>
+  );
+};
+
+export default CafeInfo;
